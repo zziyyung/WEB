c7b68e6dde32f84bc4eff3b1c11f7bd0f7ef5077
zziyyung <jiyoung99919@gmail.com>
2022-01-28 23:45:16 +0900

    jquery

--- web04-jq/resources/js/create-table.js
+++ web04-jq/resources/js/create-table.js
@@ -2,12 +2,17 @@
 
 function makeTable(elem){
 	var $table = $("<table border=1>");
+	//테이블 태그 생성
+	
+	//$(data).find("ROW")
 	
 	//컬럼 정의하기
 	for(var i =0; i<1;i++){
+	//안 넣어도 됨 ! 밑에랑 양식 맞춰주려고 만듦 
 		var $tr=$("<tr>");
 		for(var j=0; j<elem.eq(0).children().length;j++){
 			var $td=$("<td>").append(elem.eq(0).children().eq(j).prop("tagName"));
+			//<td>EMPLIYEE_ID</td>
 			$tr.append($td);
 		}
 		$table.append($tr);
@@ -18,6 +23,7 @@ function makeTable(elem){
 		var $tr=$("<tr>");
 		for(var j=0; j<elem.eq(i).children().length;j++){
 			var $td=$("<td>").append(elem.eq(i).children().eq(j).text());
+			//<td>100</td>
 			$tr.append($td);
 		}
 		$table.append($tr);
